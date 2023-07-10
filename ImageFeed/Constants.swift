41a7c65c76806eb68e355f1f97f7c3c873791ee9
AndreyAslanov <an.aslanov@yandex.ru>
2023-07-10 12:46:54 +0400

    Image Feed. 10 спринт

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Андрей Асланов on 23.06.23.
+//
+
+import Foundation
+
+let AccessKey = "Uavd3Ol50ZFUGSbAOb5hnVBDSdg6Eo7qZh-Q5Iij3Lw"
+let SecretKey = "o1JsZAfdxpWYISzD__5HKv7qFmMtx9jJrs852Dxs26Q"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
